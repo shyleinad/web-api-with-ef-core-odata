b8861dd24466f55e25cb2b1220e80fc52e046142
Dániel Túri <turi.daniel@protonmail.com>
2025-10-15 16:28:29 +0200

    Implemented product entity

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace web_api_with_ef_core_odata.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public int CategoryId { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+}
