f6f385a9d62eb5b770879517f006904e954234b4
Dániel Túri <turi.daniel@protonmail.com>
2025-10-15 16:29:03 +0200

    Implemented db context

---
+++ Data/ProductContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using web_api_with_ef_core_odata.Models;
+
+namespace web_api_with_ef_core_odata.Data;
+
+public class ProductContext : DbContext
+{
+    public DbSet<Models.Product> Products { get; set; }
+
+    public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>().HasData(
+            new Product { Id = 1, Name = "Laptop", CategoryId = 1, Price = 1200 },
+            new Product { Id = 2, Name = "Keyboard", CategoryId = 1, Price = 100 },
+            new Product { Id = 3, Name = "Chair", CategoryId = 2, Price = 200 },
+            new Product { Id = 4, Name = "Desk Lamp", CategoryId = 3, Price = 60 }
+        );
+    }
+}
